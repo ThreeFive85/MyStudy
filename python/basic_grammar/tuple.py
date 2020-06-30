6e7e1859f81d91d23c8d9437692cd36fcdb77eec
박동혁 <kaje0god@naver.com>
2020-06-30 19:20:51 +0900

    Feat : 튜플

---
+++ python/basic_grammar/tuple.py
@@ -0,0 +1,13 @@
+# 리스트와는 다르게 내용을 변경하거나, 추가하는 것이 불가능 하다. 하지만 속도는 리스트보다 빠르다.
+# 그래서 변경되지 않는 목록들을 사용할 때 활용할 수 있다.
+menu = ("돈까스", "치즈돈까스")
+print(menu[0])
+print(menu[1])
+
+name = "renerd"
+age = 30
+hobby = "game"
+print(name, age, hobby)
+
+name, age, hobby = "penny", 20, "shopping"
+print(name, age, hobby)
