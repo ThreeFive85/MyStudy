96c0c571c02e2745730605bc40a2502e07f63cad
박동혁 <kaje0god@naver.com>
2020-07-26 18:39:52 +0900

    Feat : 다중 상속  - 다중 상속에 대해

---
+++ python/basic_grammar/multipleInheritance.py
@@ -0,0 +1,56 @@
+# 다중 상속
+
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+
+# 공격 유닛
+
+
+class AttackUnit(Unit):  # Unit을 상속 받는다.
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)  # Unit의 내용을 가져온다.
+        self.damage = damage
+
+    # attack 메소드
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(
+            self.name, location, self.damage))
+
+    # damaged 메소드
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린/파이어뱃/탱크 등 지상 유닛들을 수송. 공격은 할 수 없음
+
+# 날 수 있는 기능을 가진 클래스
+
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(
+            name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+# 만약 레이스 같은 경우는 공격도 할 수 있고 날 수도 있어야 한다.
+# 그래서 AttackUnit, Flyable을 상속 받는다.
+
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
